4ac7aa53e31bc8b7d035af762cceac2eb0d0cdbc
Lucas Bigeard <luc.allau@gmail.com>
2024-11-08 14:44:15 +0100

    try catch correction to avoid databases crashes

--- app/src/main/java/com/openclassrooms/arista/data/repository/UserRepository.kt
+++ app/src/main/java/com/openclassrooms/arista/data/repository/UserRepository.kt
@@ -6,13 +6,13 @@ import com.openclassrooms.arista.domain.model.User
 
 class UserRepository(private val userDtoDao: UserDtoDao) {
 
-    suspend fun getUser(): User? {
+    suspend fun getUser(): User {
         return try {
             // Get the user
             User.fromDto(userDtoDao.getUserById(1))
         } catch (e: Exception) {
             Log.d("DatabaseError", "Error when retrieving user", e)
-            null // Return null if an exception occurs
+            return User("Empty", "placeholder")
         }
     }
 }
